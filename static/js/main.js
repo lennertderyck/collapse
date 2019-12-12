d7cc6ad946610143fefeec9f51df631b6ce9a3c5
gdm-lenndery <32176355+gdm-1718-lenndery@users.noreply.github.com>
2019-12-12 13:27:19 +0100

    Initial commit

--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,57 @@
 const app = {
     initialize() {
-        console.log('[service] main.js running!')
+        console.log()
+        console.log('\n' + `%c[service] ${arguments.callee.name}() running! \n` + ' ', 'color: #00d400; font-weight: bold');
+        this.cached();
+
+        this.collapseInit();
     },
 
     cached() {
+        console.log(`%c[service] ${arguments.callee.name}()`, 'font-weight: bold');
+
         // Put cache elements here
+        this.collapseTrigger = document.querySelectorAll('[data-collapse-trigger]');
+        this.collapseTarget = document.querySelectorAll('[data-collapse-target]');
+    },
+
+    collapseInit() {
+        console.log(`%c[service] ${arguments.callee.name}()`, 'font-weight: bold');
+
+        let targetName = '';
+        this.collapseTrigger.length !== 0 ? console.log(`\tcollapse triggers available`) : console.log(`\tno collapse triggers`);
+        this.collapseTrigger.forEach((trigger) => {
+            trigger.addEventListener("click", (() => {
+                this.collapseDo(trigger.dataset.collapseTrigger);
+                console.log(`eventlistener added ${trigger.dataset.collapseTrigger}`)
+            }));
+
+            if (trigger.classList.contains('trigger-resting') == false) {
+                console.log(`\tNo class trigger-resting`)
+                trigger.classList.add('trigger-resting');
+            }
+        });
+
+        this.collapseTarget.forEach((collapse) => {
+            if (collapse.classList.contains('collapse') == false) {
+                console.log(`\tNo class collapse`)
+                collapse.classList.add('collapse', '');
+            }
+        })
+    },
+
+    collapseDo(target) {
+        console.log(`%c[service] ${arguments.callee.name}()`, 'font-weight: bold');
+
+        target = document.querySelector(`[data-collapse-target='${target}']`);
+
+        if (target.classList.contains('show') == false) {
+            target.classList.add('show');
+            target.classList.remove('hidden');
+
+        } else {
+            target.classList.remove('show');
+            target.classList.add('hidden');
+        }
     }
 }
